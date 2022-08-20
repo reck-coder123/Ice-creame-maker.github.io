c1f29a0d265bef2fb968b00122050bb450dc9f3b
reck-coder123 <105230070+reck-coder123@users.noreply.github.com>
2022-08-20 08:23:47 +0530

    Update script.js

--- ice creme maker/script.js
+++ ice creme maker/script.js
@@ -3,26 +3,26 @@ let btn2=document.getElementById("decrement");
 
 
 
+
 function ice(){
-    arr=[6,5,4,3,2,1,1];
-    arr1=[];
-    btn1.addEventListener("click",function(){
-        for(var i=0; i<arr.length; i++){
+    var arr=[6,5,4,3,2,1];
+    var arr1=[];
+    btn1.addEventListener("click", function(){
+        let i=0;
+        
             document.getElementById("rect-"+arr[i]).style.visibility="visible";
             arr1.unshift(arr[i]);
             arr.shift();
-            break;
-    
-        }
+           
     });
 
     btn2.addEventListener("click",function(){
-        for(var j=0; j<arr1.length;j++){
+        let j=0;
+        
             document.getElementById("rect-"+arr1[j]).style.visibility="hidden";
             arr.unshift(arr1[j])
             arr1.shift();
-            break;
-        }
+       
     })
 
 }
